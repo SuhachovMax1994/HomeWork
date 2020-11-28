0d41d1d11b3bbb82f6bc4febb22ea062fef964b3
MAC <mac@MacBook-Pro-MAC-3.local>
2020-11-28 22:12:37 +0200

    version1

---
+++ les.js
@@ -0,0 +1,14 @@
+var vozrast = prompt("Введите Год рождения?");
+
+age = parseInt(vozrast);
+
+var vozrast = prompt("Введите месяц?");
+
+age = parseInt(vozrast);
+
+var vozrast = prompt("Введите день рождения?");
+
+age = parseInt(vozrast);
+
+
+
