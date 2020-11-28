594ff503afa3da73cb210bf309b35fffb53474e1
MAC <mac@MacBook-Pro-MAC-3.local>
2020-11-28 22:50:57 +0200

    Changes for a and b

--- les.js
+++ les.js
@@ -11,4 +11,24 @@ var vozrast = prompt("Введите день рождения?");
 age = parseInt(vozrast);
 
 
+var vozrast = prompt("Введите Год рождения?");
 
+age = parseInt(vozrast);
+
+var vozrast = prompt("Введите месяц?");
+
+age = parseInt(vozrast);
+
+var vozrast = prompt("Введите день рождения?");
+
+age = parseInt(vozrast);var vozrast = prompt("Введите Год рождения?");
+
+age = parseInt(vozrast);
+
+var vozrast = prompt("Введите месяц?");
+
+age = parseInt(vozrast);
+
+var vozrast = prompt("Введите день рождения?");
+
+age = parseInt(vozrast);
