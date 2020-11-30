6d4f603f63d0dc8e3462b9bac29bd698389c2216
MAC <mac@MacBook-Pro-MAC-3.local>
2020-11-30 17:07:38 +0200

    homework1

--- les.js
+++ les.js
@@ -10,25 +10,4 @@ var vozrast = prompt("Введите день рождения?");
 
 age = parseInt(vozrast);
 
-
-var vozrast = prompt("Введите Год рождения?");
-
-age = parseInt(vozrast);
-
-var vozrast = prompt("Введите месяц?");
-
-age = parseInt(vozrast);
-
-var vozrast = prompt("Введите день рождения?");
-
-age = parseInt(vozrast);var vozrast = prompt("Введите Год рождения?");
-
-age = parseInt(vozrast);
-
-var vozrast = prompt("Введите месяц?");
-
-age = parseInt(vozrast);
-
-var vozrast = prompt("Введите день рождения?");
-
-age = parseInt(vozrast);
+efefefefer
